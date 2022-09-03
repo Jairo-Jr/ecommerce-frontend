49cca31098861c9078dfebe603f0ec379635b546
Jairo-Jr <jairo.huallpa1@gmail.com>
2022-09-02 23:23:31 -0500

    select de categoria y lista de productos en la pagina de tienda

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const bar = document.getElementById('bar')
 const close = document.getElementById('close')
 const nav = document.getElementById('navbar')
+const select = document.getElementById('category-select')
+const option = document.getElementById('option')
 
 if (bar) {
     bar.addEventListener('click', () => {
@@ -12,4 +14,21 @@ if (close) {
     close.addEventListener('click', () => {
         nav.classList.remove('active')
     })
+}
+
+if (select) {
+    select.addEventListener('click', (event) => {
+        event.preventDefault()
+        if (select.classList.contains('active')) {
+            option.setAttribute('style', 'display: none;')
+            select.classList.remove('active')
+        } else {
+            option.setAttribute('style', '')
+            select.classList.add('active')
+        }
+    })
+}
+
+function clicky(idCategory) {
+    console.log('CLICKY ' + idCategory)
 }
